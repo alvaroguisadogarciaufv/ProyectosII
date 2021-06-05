a05f78075cc6508ffc9f8c87b656e6cd56dad311
Javier31498 <javi31498@gmail.com>
2021-06-05 22:40:41 +0200

    prueba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -109,10 +109,10 @@ beneficio mutuo.</p>
 </div> <!-- fin container -->
 
   
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
 
 </body>
 </html>
